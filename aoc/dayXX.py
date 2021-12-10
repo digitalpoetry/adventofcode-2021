f9987b11d8ce00e2cb9986cf8d693153e3522ad4
Jonathan Du <jdu@atlassian.com>
2021-12-10 22:23:29 +1100

    Add solution template

---
+++ aoc/dayXX.py
@@ -0,0 +1,15 @@
+from aocd.models import Puzzle
+
+
+def part1(input_data: str) -> None:
+    pass
+
+
+def part2(input_data: str) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    puzzle = Puzzle(year=2021, day=0)
+    print(part1(puzzle.input_data))
+    print(part2(puzzle.input_data))
